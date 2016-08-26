92cea64d89346512b78d1e2aaad921154427ab1c
David Kleiven <davidkleiven446@gmail.com>
2016-08-26 11:47:22 +0200

    class for parsing msh file

---
+++ inc/gmshreader.h
@@ -0,0 +1,12 @@
+#ifndef GMSH_READER_H
+#define GMSH_READER_H
+#include <string>
+#include "facet.h"
+
+class GmshReader
+{
+  public:
+    GmshReader();
+    Facet* read( const std::string& fname ) const;
+};
+#endif
---
+++ src/gmshreader.cpp
@@ -0,0 +1,106 @@
+#include "gmshreader.h"
+#include <iostream>
+#include <fstream>
+#include <cstdexcept>
+#include <stringstream>
+using namespace std;
+
+Facet* GmshReader::read( const string& fname )
+{
+  ifstream infile( fname.c_str() );
+  if ( !infile.good() )
+  {
+    string msg("Could not open file ");
+    msg += fname;
+    msh += "...";
+    throw ( runtime_error(msg) );
+  }
+
+  // Some keywords to look for
+  const string nodestart("$Nodes");
+  const string nodeend("$EndNodes");
+  const string elementstart("$Elements");  
+  string line;
+  
+  // Read until nodestart is found + 1 line
+  bool breakOnNext = false;
+  while ( getline(line, infile) )
+  {
+    if ( line.find(nodestart) != string::npos )
+    {
+      breakOnNext = true;
+    }
+    if ( breakOnNext )
+    {
+      break;
+    }
+  }
+  if ( Facet::nodesCrd.size() > 0 )
+  {
+    throw( invalide_argument("Node coordinates is already initialized...") );
+  }
+  
+  Point point;
+  unsigned int nodeNumber;
+  while( getline(line, infile) )
+  {
+    if ( line.find(nodeend) != string::npos )
+    {
+      break;
+    }
+    stringstream ss;
+    ss << line;
+    ss >> nodeNumber;
+    ss >> point.x;
+    ss >> point.y
+    ss >> point.z;
+    Facet::nodesCrd.push_back(point);
+  }
+
+  // Read until start elements is found
+  breakOnNext = false;
+  while ( getline(line, infile) )
+  {
+    if ( line.find(elementstart) != string::npos )
+    {
+      breakOnNext=true;
+    } 
+    if ( breakOnNext )
+    {
+      break;
+    }
+  }
+
+  unsigned int elementType;
+  unsigned int numberOfTags;
+  bool isFirst = true;
+
+  Facet* first = new Facet();
+  while ( getline(line, infile) )
+  {
+    stringstream ss;
+    ss << line;
+    ss >> elementType;
+    ss >> numberOfTags;
+    unsigned int tag;
+    for ( unsigned int i=0;i<numberOfTags; i++ )
+    {
+      ss >> tag;
+    } 
+    unsigned int nodenum[3];
+    ss >> nodenum[0];
+    ss >> nodenum[1];
+    ss >> nodenum[2];
+    if ( isFirst )
+    {
+      first->setNodes(nodenum);
+      isFirst = false;
+    }
+    else
+    {
+      first = first->addFacet( nodenum );
+    }
+  }
+  infile.close();
+  return first;
+}
