8ee9015d8a8c88d5fbbc2b11d4f46750a4c906c3
David Kleiven <davidkleiven446@gmail.com>
2016-09-02 21:42:19 +0200

    Excepiton handling in unit test

--- unittest/testgmsh.cpp
+++ unittest/testgmsh.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 #include "gmshreader.h"
 #include <string>
+#include <iostream>
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE( testread )
@@ -8,16 +9,31 @@ BOOST_AUTO_TEST_CASE( read )
 {
   const string fname("squareAcc.msh");
   GmshReader reader;
-  Facet* facets = reader.read( fname );
-  BOOST_CHECK_EQUAL( Facet::nodesCrd.size(), 45 );
-  
-  // Count facets
-  unsigned int counter = 0;
-  for ( Facet* iter=facets; iter != NULL; iter=iter->next() )
+  try
   {
-    counter++;
-  } 
-  BOOST_CHECK_EQUAL( counter, 88 ); 
-  delete facets;
+    Facet* facets = reader.read( fname );
+    BOOST_CHECK_EQUAL( Facet::nodesCrd.size(), 45 );
+    
+    // Count facets
+    unsigned int counter = 0;
+    for ( Facet* iter=facets; iter != NULL; iter=iter->next() )
+    {
+      counter++;
+    } 
+    BOOST_CHECK_EQUAL( counter, 88 ); 
+    delete facets;
+  }
+  catch ( runtime_error &exc )
+  {
+    cout << exc.what() << endl;
+  }
+  catch( invalid_argument &exc )
+  {
+    cout << exc.what() << endl;
+  }
+  catch(...)
+  {
+    cout << "Unexpected excepiton...\n";
+  }
 }
 BOOST_AUTO_TEST_SUITE_END()
