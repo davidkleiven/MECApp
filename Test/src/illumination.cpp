bff44027930f240532bfc6b4a0272b0256699451
David Kleiven <davidkleiven446@gmail.com>
2016-09-06 11:25:16 +0200

    Finished program

--- Test/src/illumination.cpp
+++ Test/src/illumination.cpp
@@ -1,5 +1,34 @@
+#include "gmshreader.h"
+#include "facets.h"
+#include "point.h"
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
 int main( int argc, char **argv )
 {
-    
+  string fname("sphere.msh");
+  string fnameOut("data/illumintedSphere.vtp");
+  
+  // Initialize the GMSH reader
+  GmshReader reader;
+  
+  // Parse the file and get a facets object
+  try
+  {
+    Facets facets = reader.read(fname); 
+    Vec3 sourcePosition(-10.0,0.0,0.0);
+    facets.computeDistanceFromSource( sourcePosition );
+    facets.sortByDistanceFromSource();
+    facets.illuminate();
+    facets.saveIlluminationVTK( fnameOut );
+  }
+  catch ( exception &exc )
+  {
+    cerr << exc.what() << endl;
+    return 1;
+  }
+
   return 0;
 } 
